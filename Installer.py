2fbea2a83e11a78592f12d94339e87aab17158f2
Justin Arnold <58081163+JustinTArnold@users.noreply.github.com>
2021-01-29 12:41:21 -0500

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -14,29 +14,29 @@ try:
 	print ("csv_templates directory created")
 except:
 	print ("csv_templates directory already exists")
-
+time.sleep(1)
 for each in all_files:
 	if each.endswith('csv'):
 		os.rename(each, f'./csv_templates/{each}')
 print('moved all csv template files')
-
+time.sleep(1)
 try:
-	my_path = os.path.join('./', 'outpot')
+	my_path = os.path.join('./', 'output')
 	os.mkdir(my_path)
 	print ("output directory created")
 except:
 	print ("output directory already exists")
-
+time.sleep(1)
 try:
 	my_path = os.path.join('./', 'update_files')
 	os.mkdir(my_path)
 	print ("update_files directory created")
 except:
 	print ("update_files directory already exists")
-
+time.sleep(1)
 print ("installing dependencies")
 time.sleep(2)
-for each in ('pyqt5', 'csv', 'numpy', 'panas', 'sys', 'threading', 'datetime', 'wget'):
+for each in ('pyqt5', 'numpy', 'pandas', 'sys', 'datetime', 'wget'):
 	subprocess.run(f'pip install {each}')
 print('all dependencies installed: hopefully')
 break_line = input("<<<Press any key to exit>>>")
