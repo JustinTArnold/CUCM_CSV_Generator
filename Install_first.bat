59ebb2d6c1b5ac16d826d7c6cc3340bd709374e2
Justin Arnold <58081163+JustinTArnold@users.noreply.github.com>
2021-01-29 12:28:37 -0500

    Update Install_first.bat

--- Install_first.bat
+++ Install_first.bat
@@ -1,6 +1,27 @@
 cd /D "%~dp0"
 rename *.exe python_installer.exe
 start python_installer.exe /quiet PrependPath=1 Include_test=0
+echo installing python
+10
+sleep 1
+9
+sleep 1
+8
+sleep 1
+7
+sleep 1
+6
+sleep 1
+5
+sleep 1
+4
+sleep 1
+3
+sleep 1
+2
+sleep 1
+1
+sleep1
 echo Pyhton Installed
 pause
 start installer.py
