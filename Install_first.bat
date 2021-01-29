07d728665a71181d7f2f46671ae994c09c30e460
Justin Arnold <58081163+JustinTArnold@users.noreply.github.com>
2021-01-29 12:40:04 -0500

    Update Install_first.bat

--- Install_first.bat
+++ Install_first.bat
@@ -4,8 +4,8 @@ rename *.exe python_installer.exe
 start python_installer.exe /quiet PrependPath=1 Include_test=0
 echo installing python
 timeout /t 5
-echo Pyhton Installed
-pause
+echo Python Installed
+timeout /t 2
 python installer.py
 pause
 
