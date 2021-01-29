979ebee208b17e70f36d0974fa6b2551a1485148
Justin Arnold <58081163+JustinTArnold@users.noreply.github.com>
2021-01-29 11:22:16 -0500

    Add files via upload

---
+++ Installer_Updater.py
@@ -0,0 +1,51 @@
+import subprocess
+import os
+from os import listdir, mkdir
+from os.path import isfile, join
+import time
+
+my_path = "./"
+all_files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+print ("installing python")
+for each in all_files:
+	if each.endswith('exe'):
+		os.rename(each,'python_installer.exe')
+subprocess.run('python_installer.exe /quiet PrependPath=1 Include_test=0')
+print ("python installed")
+os.remove('python_installer')
+
+try:
+	my_path = os.path.join('./', 'csv_templates')
+	os.mkdir(my_path)
+	print ("csv_templates directory created")
+except:
+	print ("csv_templates directory already exists")
+
+for each in all_files:
+	if each.endswith('csv'):
+		os.rename(each, f'./csv_templates/{each}')
+print('moved all csv template files')
+
+try:
+	my_path = os.path.join('./', 'outpot')
+	os.mkdir(my_path)
+	print ("output directory created")
+except:
+	print ("output directory already exists")
+
+try:
+	my_path = os.path.join('./', 'update_files')
+	os.mkdir(my_path)
+	print ("update_files directory created")
+except:
+	print ("update_files directory already exists")
+
+print ("installing dependencies")
+time.sleep(2)
+for each in ('pyqt5', 'csv', 'numpy', 'panas', 'sys', 'threading', 'datetime', 'wget'):
+	subprocess.run(f'pip install {each}')
+print('all dependencies installed: hopefully')
+break_line = input("<<<Press any key to exit>>>")
+
+
+
