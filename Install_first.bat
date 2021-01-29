9eed779799911c092dbcdf2d543aa25220a3e605
Justin Arnold <58081163+JustinTArnold@users.noreply.github.com>
2021-01-29 12:20:41 -0500

    Update Install_first.bat

--- Install_first.bat
+++ Install_first.bat
@@ -1,3 +1,7 @@
+python_installer.exe /quiet PrependPath=1 Include_test=0
 rename *.exe python_installer.exe
 start python_installer.exe /quiet PrependPath=1 Include_test=0
+echo Pyhton Installed
+pause
 start installer.py
+
